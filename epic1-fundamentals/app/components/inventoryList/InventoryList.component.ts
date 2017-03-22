3b53a0bd184b8dae1c65ffff2afa733792b2374b
Bobby Tanwer <btanwer-aa@parago.com>
2017-03-22 18:14:41 -0500

    review

---
+++ epic1-fundamentals/app/components/inventoryList/InventoryList.component.ts
@@ -0,0 +1,14 @@
+/**
+ * Created by btanwer on 3/21/2017.
+ */
+
+import { Component } from '@angular/core';
+
+@Component({
+    selector:'inventory-list',
+    templateUrl:'InventoryList.component.html'
+})
+
+export class InventoryList{
+
+}
---
+++ epic1-fundamentals/app/services/InventoryService.service.ts
@@ -0,0 +1,3 @@
+/**
+ * Created by btanwer on 3/15/2017.
+ */
